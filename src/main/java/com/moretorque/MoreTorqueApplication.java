1aaa37b6329506cc97ca147aae918ed726a04e9f
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2024-08-12 16:58:04 +0530

    first commit

---
+++ src/main/java/com/moretorque/MoreTorqueApplication.java
@@ -0,0 +1,13 @@
+package com.moretorque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreTorqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoreTorqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moretorque/MoreTorqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moretorque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreTorqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
